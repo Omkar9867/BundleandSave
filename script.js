662c904490242633f31e90b2697d8245d70947a1
Omkar <omkarrajan495@gmail.com>
2024-02-09 23:46:10 +0530

    Modified files replaces

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 const radioButtons = document.querySelectorAll('input[type="radio"]');
+const totalAmount = document.querySelector('.value .amount');
 
-radioButtons.forEach(radioButton => {
-    radioButton.addEventListener('click', (event) => {
-        const boxes = document.querySelectorAll('.main');
-        boxes.forEach(box => {
-            box.classList.remove('active');
-        });
-
-        const selectedBox = event.target.closest('.main');
-        selectedBox.classList.add('active');
+// Function to handle box click event
+const handleBoxClick = (event) => {
+    const clickedBox = event.currentTarget;
+    const radioButton = clickedBox.querySelector('input[type="radio"]');
+    if (!radioButton.checked) {
+        radioButton.checked = true;
+        const selectedAmt = clickedBox.querySelector('.align-inside strong').innerText;
+        updateTotalAmount(selectedAmt);
+    }
+    const boxes = document.querySelectorAll('.main');
+    boxes.forEach(box => {
+        box.classList.remove('active');
     });
+    clickedBox.classList.add('active');
+};
 
-}
-);
+// Function to update total amount
+const updateTotalAmount = (selectedAmt) => {
+    totalAmount.innerText = selectedAmt;
+};
+
+
+// Event listener for box click
+document.querySelectorAll('.main').forEach(box => {
+    box.addEventListener('click', handleBoxClick);
+});
